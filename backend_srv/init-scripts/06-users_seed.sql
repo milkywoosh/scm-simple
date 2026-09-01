1bb5b2ef4de8fdd18828381abb25dbafd4c49396
luke_aul <auliya.lukman@sigma.co.id>
2026-09-02 06:04:20 +0700

    add created at field

--- backend_srv/init-scripts/06-users_seed.sql
+++ backend_srv/init-scripts/06-users_seed.sql
@@ -8,7 +8,8 @@ CREATE TABLE users (
     full_name varchar(100),
     email varchar(90) UNIQUE,
     password_changed_at TIMESTAMPTZ default null,
-    created_at TIMESTAMPTZ
+    created_at TIMESTAMPTZ,
+    
 );
 
 CREATE INDEX idx_users_username_email
